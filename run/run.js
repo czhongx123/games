68f7d64c4309664394439771e505b5b62fda6145
czhongx123 <792006914@qq.com>
2019-01-05 18:52:26 +0800

    update

--- run/run.js
+++ run/run.js
@@ -16,7 +16,22 @@ function run(option){
     let speed= '50'||option.speed;
     let direction='left'|| option.direction;
     let data=option.data;
-    
+    let str='';
+    for(let i=0;i<data.length;i++){
+        str+='<p class="pcs">'+data[i]+'</p>'
+    }
+    console.log(el)
+    document.getElementById(el).innerHTML=str;
+    document.getElementById(el).style.position='relative';
+    // for(let j=0;j<document.getElementsByClassName('pcs').length;j++){
+    //     document.getElementsByClassName('pcs')[j].style.marginRight="200px";
+    //     document.getElementsByClassName('pcs')[j].style.position="absolute";
+    // }
+
+
 
 }
-run()
+run({
+    el:'container',
+    data:['哈哈哈哈哈','快快快看']
+})

a98874cd86191328dbf897b276d3845705c78c6e
czhongx123 <792006914@qq.com>
2019-03-19 15:44:23 +0800

    更新

--- sudoku/src/js/toolkit.js
+++ sudoku/src/js/toolkit.js
@@ -20,7 +20,7 @@ const matrixToolkit={
         const endIndex=array.length-2;
         for(let i=0;i<=endIndex;i++){
             const j=i+Math.floor(Math.random()*(array.length-i));
-            [array[i].array[j]]=[array[j].array[i]]
+            [array[i],array[j]]=[array[j],array[i]]
         }
         return array
     }
---
+++ test/t1.js
@@ -0,0 +1,4 @@
+let arr=[1,2,3];
+const ar=Array.from({length:9},()=>(arr));
+console.log(ar)
+console.log(arr)

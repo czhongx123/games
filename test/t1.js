189c2009c7a60d42164967dc46af3754dcd38248
czhongx123 <792006914@qq.com>
2019-04-04 18:11:05 +0800

    更新运动

--- test/t1.js
+++ test/t1.js
@@ -1,4 +1,4 @@
-let arr=[1,2,3];
-const ar=Array.from({length:9},()=>(arr));
-console.log(ar)
-console.log(arr)
+// let arr=[1,2,3];
+// const ar=Array.from({length:9},()=>(arr));
+// console.log(ar)
+// console.log(arr)
